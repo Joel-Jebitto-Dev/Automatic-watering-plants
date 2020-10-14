2a4fc6d1286fb4ed6683927f15696f7848d1eaf1
Obed Otto <obedotto@gmail.com>
2020-10-14 14:23:45 +0530

    created project

---
+++ app/src/main/java/automatic_watering_plants/joeljebitto/com/MainActivity.java
@@ -0,0 +1,14 @@
+package automatic_watering_plants.joeljebitto.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
